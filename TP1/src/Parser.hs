3f01332f06b9bd91c293eff8c7850052a74a3e37
ernestosavio <ernesto_savio@hotmail.com>
2025-09-26 18:04:24 -0300

    intexp parser

--- TP1/src/Parser.hs
+++ TP1/src/Parser.hs
@@ -45,7 +45,57 @@ lis = makeTokenParser
 --- Parser de expresiones enteras
 -----------------------------------
 intexp :: Parser (Exp Int)
-intexp = undefined
+intexp = try (term `chainl1` plusExp) 
+         <|> 
+         try (term `chainl1` minusExp) 
+         <|>
+         term
+  where
+  plusExp :: Parser (Exp Int -> Exp Int -> Exp Int)
+  plusExp = do { reservedOp lis "+";  return Plus }
+
+  minusExp :: Parser (Exp Int -> Exp Int -> Exp Int)
+  minusExp = do { reservedOp lis "-";  return (Minus) }
+
+
+term :: Parser (Exp Int)
+term = try (unary `chainl1` timesExp) 
+       <|> 
+       try (unary `chainl1` divExp) 
+       <|>
+       unary  
+  where
+  timesExp :: Parser (Exp Int -> Exp Int -> Exp Int)
+  timesExp = do { reservedOp lis "*";  return Times }
+
+  divExp :: Parser (Exp Int -> Exp Int -> Exp Int)
+  divExp = do { reservedOp lis "/";  return Div }
+
+unary :: Parser (Exp Int)
+unary = try (do { reservedOp lis "-"; a <- atom; return (UMinus a) }) <|> atom
+  where
+  uMinusExp :: Parser (Exp Int -> Exp Int)
+  uMinusExp = do { reservedOp lis "-";  return UMinus }
+
+atom :: Parser (Exp Int)
+atom = try nat <|> try (varExp) <|> (parens lis intexp)
+  where
+  nat :: Parser (Exp Int)
+  nat = do {
+            n <- natural lis;
+            return (Const (fromIntegral n))
+           }
+  varExp :: Parser (Exp Int)
+  varExp = do {
+                v <- identifier lis;
+                try (do { reservedOp lis "+";
+                          reservedOp lis "+";
+                          return (VarInc v) 
+                        }) 
+                <|>
+                return (Var v)
+              }
+
 
 ------------------------------------
 --- Parser de expresiones booleanas
